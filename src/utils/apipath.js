377a3eb43cb57cfbc4bc215c6523f0e047ea9bbd
laiwola <layiibrahim32@gmail.com>
2026-03-02 07:01:55 +0100

    api_path added

---
+++ src/utils/apipath.js
@@ -0,0 +1,25 @@
+export const API_PATH={
+    AUTH:{
+        REGISTER:"/api/auth/register",
+        LOGIN:"/api/auth/login",
+        GET_PROFILE:"/api/auth/user-profile",
+        UPDATE_PROFILE:"/api/auth/update",
+        VERIFY_EMAIL:"/api/auth/verify-email",
+        RESEND_OTP:"/api/auth/resend-otp"
+    }
+    // BOOKS:{
+    //     CREATE_BOOKS:"/api/book",
+    //     GET_BOOKS:"/api/book",
+    //     GET_BOOK_ID:"/api/book",
+    //     UPDATE_BOOK:"/api/book",
+    //     UPDATE_BOOK_COVER:"/api/book/cover"
+    // },
+    // AI:{
+    //     GENERATE_OUTLINE:"/api/ai/generate-outline",
+    //     GENERATE_CONTENT:"/api/ai/generate-chapter-content"
+    // },
+    // EXPORT:{
+    //     PDF:"/api/export",
+    //     DOC:"/api/export"
+    // }
+}
